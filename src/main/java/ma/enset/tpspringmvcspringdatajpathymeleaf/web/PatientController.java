a6ad30bb37fd3fa536654204958151a0f67e0d6f
Soukac <soukainaaitchrif2004@gmail.com>
2025-04-14 10:17:14 +0100

    Pagination

--- src/main/java/ma/enset/tpspringmvcspringdatajpathymeleaf/web/PatientController.java
+++ src/main/java/ma/enset/tpspringmvcspringdatajpathymeleaf/web/PatientController.java
@@ -4,9 +4,12 @@ package ma.enset.tpspringmvcspringdatajpathymeleaf.web;
 import lombok.AllArgsConstructor;
 import ma.enset.tpspringmvcspringdatajpathymeleaf.entitites.Patient;
 import ma.enset.tpspringmvcspringdatajpathymeleaf.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,13 @@ public class PatientController {
 
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("Listpatients", patients);
+    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page ,
+                        @RequestParam(name="size",defaultValue = "4") int size) {
+       // List<Patient> patients = patientRepository.findAll();
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+
+        model.addAttribute("Listpatients", patients.getContent());
+        model.addAttribute("pages",new int[patients.getTotalPages()]);
         return "patients";
     }
 }
